ef498d0fe47dc23051bcdb2bd31f94e89149a502
Faizal <144884174+Alter-Lexcy@users.noreply.github.com>
2024-11-08 21:31:24 +0700

    Membuat Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\AssessmentController;
+use App\Http\Controllers\ClassesController;
+use App\Http\Controllers\CollectionController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ModulController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TeacherController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,5 +15,17 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+route::middleware('auth')->group(function(){
+    route::resource([
+        'Teacher'=>TeacherController::class,
+        'Class'=>ClassesController::class,
+        'Modul'=>ModulController::class,
+        'Task'=>TaskController::class,
+        'Collection'=>CollectionController::class,
+        'Assessment'=>AssessmentController::class,
+        'Comment'=>CommentController::class
+    ]);
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
