4e002c3b3c83fc645646d2dac619165a0d645de1
davian27 <daviannaufalfadhil@gmail.com>
2024-11-12 11:58:26 +0700

    updated validasi

--- app/Http/Controllers/AssessmentController.php
+++ app/Http/Controllers/AssessmentController.php
@@ -14,6 +14,7 @@ class AssessmentController extends Controller
     public function index()
     {
         $assessment = Assessment::with('collections')->get();
+        // dd
 
         return view('Admins.Assesments.index', compact('assessment'));
     }
