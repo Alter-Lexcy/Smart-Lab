b39148ea15d840d4036b298400a6275b6221d5ff
nazriel040608 <acmassirau@gmail.com>
2024-11-14 22:15:59 +0700

    h

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,21 @@
 
     <!-- Preline Select JS -->
     <script src="https://cdn.jsdelivr.net/npm/preline@latest/dist/preline.min.js"></script>
+    <script>
+        window.addEventListener('load', function() {
+            setTimeout(function() {
+                // Tambahkan class 'slide-out-up' untuk memulai transisi
+                document.getElementById('preloader').classList.add('transform', '-translate-y-full');
+
+                // Hapus preloader setelah transisi selesai (misal 700ms)
+                setTimeout(function() {
+                    document.getElementById('preloader').style.display = 'none';
+                    document.getElementById('content').classList.remove('opacity-0');
+                    document.getElementById('content').classList.add('opacity-100');
+                }, 700); // Sesuaikan dengan durasi transisi
+            }, 700); // Durasi preloader ditampilkan (3000ms = 3 detik)
+        });
+    </script>
 </head>
 
 <body class="bg-gray-100 font-sans antialiased">
@@ -222,13 +237,17 @@
         <div class="flex-1 p-4 ml-14">
             <nav class="flex items-center justify-between bg-white p-4 shadow-lg rounded-lg relative z-10">
                 <div class="ml-auto relative flex">
+                    <!-- Inbox Icon Button -->
                     <button class="mr-4 border-2 rounded-lg p-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" />
-                          </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="size-5">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z" />
+                        </svg>
                     </button>
+
                     <!-- User Icon Button -->
-                    <button id="userDropdown" class="text-gray-700 mr-5 ">
+                    <button id="userDropdown" onclick="toggleDropdown()" class="text-gray-700 mr-5">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
                             class="size-8 rounded-3xl">
                             <path fill-rule="evenodd"
@@ -239,7 +258,7 @@
 
                     <!-- User Profile Dropdown -->
                     <div id="dropdownMenu"
-                        class="absolute right-0 mt-14 w-80 bg-white rounded-lg shadow-lg opacity-0 invisible transition-all duration-300">
+                        class="absolute right-0 mt-14 w-80 bg-white rounded-lg shadow-lg transition-all duration-300 hidden">
                         <div class="py-2 px-4">
                             <h5 class="text-xl mt-2 ml-2 font-semibold">User Profile</h5>
                         </div>
@@ -247,11 +266,12 @@
                             <img src="https://pkl.hummatech.com/user.webp" class="rounded-3xl" width="80"
                                 height="80" alt="">
                             <div class="ms-3">
-                                <h5 class="mb-1 font-medium"><span class="uppercase ">{{ Auth::user()->name }}</span>
+                                <h5 class="mb-1 font-medium"><span class="uppercase">{{ Auth::user()->name }}</span>
                                 </h5>
                                 <span class="mb-2 text-md">Guru</span>
                                 <p class="mt-1 text-sm flex items-center gap-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-4">
                                         <path stroke-linecap="round" stroke-linejoin="round"
                                             d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                                     </svg>
@@ -267,7 +287,7 @@
                                 Logout
                             </a>
 
-                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" class="d-none">
+                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" class="hidden">
                                 {{ csrf_field() }}
                             </form>
                         </div>
@@ -275,7 +295,30 @@
                 </div>
             </nav>
 
-            @yield('content')
+            <script>
+                function toggleDropdown() {
+                    const dropdownMenu = document.getElementById("dropdownMenu");
+                    dropdownMenu.classList.toggle("hidden");
+                }
+
+                // Close dropdown if clicked outside
+                document.addEventListener("click", function(event) {
+                    const dropdownButton = document.getElementById("userDropdown");
+                    const dropdownMenu = document.getElementById("dropdownMenu");
+
+                    if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+                        dropdownMenu.classList.add("hidden");
+                    }
+                });
+            </script>
+
+            <main class="flex-1 transition-all duration-300 ease-in-out">
+                <div id="preloader"
+                    class="fixed inset-0 flex items-center justify-center bg-white z-50 transition-transform duration-700 transform">
+                    <img src="{{ asset('image/logo.png') }}" alt="Loading" class="h-20 w-20 animate-shake">
+                </div>
+                @yield('content')
+            </main>
         </div>
     </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,66 @@ export default {
     ],
     theme: {
         extend: {
+            keyframes: {
+                tada: {
+                    "0%": {
+                        transform: "scale3d(1, 1, 1)",
+                    },
+                    "10%, 20%": {
+                        transform: "scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -3deg)",
+                    },
+                    "30%, 50%, 70%, 90%": {
+                        transform: "scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 3deg)",
+                    },
+                    "40%, 60%, 80%": {
+                        transform: "scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -3deg)",
+                    },
+                    "100%": {
+                        transform: "scale3d(1, 1, 1)",
+                    },
+                },
+                shake: {
+                    "0%, 100%": {
+                        transform: "translateX(0)",
+                    },
+                    "10%, 30%, 50%, 70%, 90%": {
+                        transform: "translateX(-5px)",
+                    },
+                    "20%, 40%, 60%, 80%": {
+                        transform: "translateX(5px)",
+                    },
+                },
+                slideInUp: {
+                    '0%': {
+                        transform: 'translateY(100%)',
+                        backgroundColor: 'gray',
+                    },
+                    '100%': {
+                        transform: 'translateY(0)',
+                        backgroundColor: 'transparent',
+                    },
+                },
+                bounce: {
+                    '0%, 20%, 50%, 80%, 100%': {
+                        transform: 'translateY(0)',
+                    },
+                    '40%': {
+                        transform: 'translateY(-30px)',
+                    },
+                    '60%': {
+                        transform: 'translateY(-15px)',
+                    },
+                },
+            },
+            animation: {
+                tada: 'tada 1s ease-in-out 0.25s 1',
+                shake: 'shake 0.6s ease-in-out 0.25s 1',
+                slideInUp: 'slideInUp 0.5s ease-out',
+                bounce: 'bounce 1s infinite',
+            },
             colors: {
                 cornflowerblue: '#6495ED',
-              },
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
                 poppins: ['Poppins', 'sans-serif'], // Tambahkan Poppins sebagai font kustom
