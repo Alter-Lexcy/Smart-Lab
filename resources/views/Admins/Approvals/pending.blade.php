68f580523cea38e794f11b4ef71216c912a9fd0b
davian27 <daviannaufalfadhil@gmail.com>
2024-11-19 21:26:45 +0700

    updated fixed register guru

--- resources/views/Admins/Approvals/pending.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container text-center">
-    <h2>Akun Anda Belum Disetujui</h2>
-    <p>Mohon tunggu hingga superadmin menyetujui akun Anda.</p>
-</div>
-@endsection
