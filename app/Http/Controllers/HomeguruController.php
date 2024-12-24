45a90c26f225fd637c2498febaef52971e885f1b
nazriel040608 <acmassirau@gmail.com>
2024-12-24 17:53:39 +0700

    paginate modal selesai

--- app/Http/Controllers/HomeguruController.php
+++ app/Http/Controllers/HomeguruController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Classes;
 use App\Models\TeacherClass;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -30,11 +31,28 @@ class HomeguruController extends Controller
 
         $students = [];
         foreach ($teacherClasses as $teacherClass) {
-            // Get students for each class
-            $students[$teacherClass->class->id] = $teacherClass->class->users()->paginate(2);
+            // Mengurutkan siswa berdasarkan nama dan melakukan pagination
+            $students[$teacherClass->class->id] = $teacherClass->class->users()->orderBy('name')->paginate(10);
         }
 
         return view('Guru.dashboardGuru', compact('teacherClasses', 'students'));
+    }
+
+    public function getClassDetails(Request $request, $classId)
+    {
+        $class = Classes::find($classId);
+
+        if (!$class) {
+            return response()->json(['message' => 'Kelas tidak ditemukan'], 404);
+        }
+
+        $students = $class->users()->get();
+
+        if ($request->ajax()) {
+            return view('partials.studentList', compact('students'))->render();
+        }
+
+        return response()->json(['message' => 'Permintaan tidak valid'], 400);
     }
 
 
